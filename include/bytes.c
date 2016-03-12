289bf56f77ac70cc9f49ae66b7c388910aaea249
Shraman Biswas <shraman-biswas@users.noreply.github.com>
2016-03-12 18:42:11 +0530

    edited comments

--- include/bytes.c
+++ include/bytes.c
@@ -1,6 +1,6 @@
 #include "bytes.h"
 
-/* gets byte buffer of specified size from specified file stream */
+/* get byte buffer of specified size from specified file stream */
 void fgetb(uint8_t *s, uint32_t size, FILE *stream)
 {
 	if (!stream) {
@@ -10,7 +10,7 @@ void fgetb(uint8_t *s, uint32_t size, FILE *stream)
 	while (((*s++ = fgetc(stream)) != EOF) && (size--));
 }
 
-/* prints byte buffer of specified size with specified byte packing width */
+/* print byte buffer of specified size with specified byte packing width */
 void printb(uint8_t *s, uint32_t size, int pkng)
 {
 	while (size)
