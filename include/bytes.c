2d0bb0203806f552edb172700ffce13ba6203251
Shraman Biswas <shraman-biswas@users.noreply.github.com>
2016-03-14 17:21:39 +0530

    using fread() instead of custom fgetb()

--- include/bytes.c
+++ include/bytes.c
@@ -1,15 +1,5 @@
 #include "bytes.h"
 
-/* get byte buffer of specified size from specified file stream */
-void fgetb(uint8_t *bytes, int size, FILE *stream)
-{
-	if (!stream) {
-		fprintf(stderr, "stream is null!\n");
-		return;
-	}
-	while (((*bytes++ = fgetc(stream)) != EOF) && (size--));
-}
-
 /* print byte buffer of specified size with specified byte packing width */
 void printb(uint8_t *bytes, int size, int pkng)
 {
